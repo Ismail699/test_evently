0b4651dea270d04438ee72ed4aaa831e0b82b3c4
John Doe <ismailmarouani20@gmail.com>
2024-04-23 23:44:18 +0200

    finished create events

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,22 +1,69 @@
-import EventForm from "@/components/shared/EventForm"
-import { auth } from "@clerk/nextjs";
+"use client";
+import React, { useState } from "react";
+import EventForm from "@/components/shared/EventForm";
+
+import { useAuth, useUser } from "@clerk/nextjs";
 
 const CreateEvent = () => {
-  const { sessionClaims } = auth();
+  // // const { sessionClaims } = auth();
+  // const [userId, setUserId] = useState("");
 
-  const userId = sessionClaims?.userId as string;
+  // const fetchUserId = async () => {
+  //   try {
+  //     const id = await userID();
+  //     setUserId(id);
+  //   } catch (error) {
+  //     console.error("Error fetching user ID:", error);
+  //   }
+  // };
 
+  // fetchUserId();
+  // console.log("userid : ", userId);
+  const { user } = useUser();
+  const id = user?.publicMetadata.userId as string;
+  console.log("userid (create event): ", id);
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Create Event
+        </h3>
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Create" />
+        <EventForm userId={id} type="Create" />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CreateEvent
+export default CreateEvent;
+
+// import EventForm from "@/components/shared/EventForm";
+// import { userID } from "@/lib/utils";
+// import { auth } from "@clerk/nextjs";
+// import { currentUser } from "@clerk/nextjs/server";
+
+// const CreateEvent = () => {
+//   const { sessionClaims } = auth();
+
+//   // const userId = sessionClaims?.userId as string;
+//   const currentuser = currentUser();
+//   const userId = userID();
+//   console.log(userId);
+//   return (
+//     <>
+//       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+//         <h3 className="wrapper h3-bold text-center sm:text-left">
+//           Create Event
+//         </h3>
+//       </section>
+
+//       <div className="wrapper my-8">
+//         <EventForm userId={userId} type="Create" />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default CreateEvent;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,66 +1,78 @@
-import { type ClassValue, clsx } from 'clsx'
+import { type ClassValue, clsx } from "clsx";
 
-import { twMerge } from 'tailwind-merge'
-import qs from 'query-string'
-
-import { UrlQueryParams, RemoveUrlQueryParams } from '@/types'
+import { twMerge } from "tailwind-merge";
+import qs from "query-string";
+import { getAuth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { UrlQueryParams, RemoveUrlQueryParams } from "@/types";
+import { connectToDatabase } from "./database";
+import User from "./database/models/user.model";
+import { createUser, updateUser } from "./actions/user.actions";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const formatDateTime = (dateString: Date) => {
   const dateTimeOptions: Intl.DateTimeFormatOptions = {
-    weekday: 'short', // abbreviated weekday name (e.g., 'Mon')
-    month: 'short', // abbreviated month name (e.g., 'Oct')
-    day: 'numeric', // numeric day of the month (e.g., '25')
-    hour: 'numeric', // numeric hour (e.g., '8')
-    minute: 'numeric', // numeric minute (e.g., '30')
+    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    month: "short", // abbreviated month name (e.g., 'Oct')
+    day: "numeric", // numeric day of the month (e.g., '25')
+    hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
     hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
-  }
+  };
 
   const dateOptions: Intl.DateTimeFormatOptions = {
-    weekday: 'short', // abbreviated weekday name (e.g., 'Mon')
-    month: 'short', // abbreviated month name (e.g., 'Oct')
-    year: 'numeric', // numeric year (e.g., '2023')
-    day: 'numeric', // numeric day of the month (e.g., '25')
-  }
+    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    month: "short", // abbreviated month name (e.g., 'Oct')
+    year: "numeric", // numeric year (e.g., '2023')
+    day: "numeric", // numeric day of the month (e.g., '25')
+  };
 
   const timeOptions: Intl.DateTimeFormatOptions = {
-    hour: 'numeric', // numeric hour (e.g., '8')
-    minute: 'numeric', // numeric minute (e.g., '30')
+    hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
     hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
-  }
+  };
 
-  const formattedDateTime: string = new Date(dateString).toLocaleString('en-US', dateTimeOptions)
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateTimeOptions
+  );
 
-  const formattedDate: string = new Date(dateString).toLocaleString('en-US', dateOptions)
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateOptions
+  );
 
-  const formattedTime: string = new Date(dateString).toLocaleString('en-US', timeOptions)
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    timeOptions
+  );
 
   return {
     dateTime: formattedDateTime,
     dateOnly: formattedDate,
     timeOnly: formattedTime,
-  }
-}
+  };
+};
 
-export const convertFileToUrl = (file: File) => URL.createObjectURL(file)
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
 
 export const formatPrice = (price: string) => {
-  const amount = parseFloat(price)
-  const formattedPrice = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format(amount)
+  const amount = parseFloat(price);
+  const formattedPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
 
-  return formattedPrice
-}
+  return formattedPrice;
+};
 
 export function formUrlQuery({ params, key, value }: UrlQueryParams) {
-  const currentUrl = qs.parse(params)
+  const currentUrl = qs.parse(params);
 
-  currentUrl[key] = value
+  currentUrl[key] = value;
 
   return qs.stringifyUrl(
     {
@@ -68,15 +80,18 @@ export function formUrlQuery({ params, key, value }: UrlQueryParams) {
       query: currentUrl,
     },
     { skipNull: true }
-  )
+  );
 }
 
-export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryParams) {
-  const currentUrl = qs.parse(params)
+export function removeKeysFromQuery({
+  params,
+  keysToRemove,
+}: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(params);
 
-  keysToRemove.forEach(key => {
-    delete currentUrl[key]
-  })
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
 
   return qs.stringifyUrl(
     {
@@ -84,10 +99,87 @@ export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryPara
       query: currentUrl,
     },
     { skipNull: true }
-  )
+  );
 }
 
 export const handleError = (error: unknown) => {
-  console.error(error)
-  throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
+  console.error(error);
+  throw new Error(typeof error === "string" ? error : JSON.stringify(error));
+};
+
+export async function SyncUser() {
+  await currentUser()
+    .then(async (currentClerkUser) => {
+      // console.log(currentClerkUser);
+      if (currentClerkUser != null) {
+        const user = {
+          clerkId:
+            currentClerkUser?.id.toString() !== undefined
+              ? currentClerkUser?.id.toString()
+              : "id",
+          email: currentClerkUser?.emailAddresses[0]?.emailAddress
+            ? currentClerkUser.emailAddresses[0].emailAddress
+            : "emailAddress",
+          username: currentClerkUser?.username
+            ? currentClerkUser?.username!
+            : "username",
+          firstName: currentClerkUser?.firstName
+            ? currentClerkUser?.firstName
+            : "firstName",
+          lastName: currentClerkUser?.lastName
+            ? currentClerkUser?.lastName!
+            : "lastName",
+          photo: currentClerkUser?.imageUrl
+            ? currentClerkUser?.imageUrl!
+            : "photo",
+        };
+
+        const newUser = await createUser(user);
+
+        if (newUser) {
+          await clerkClient.users.updateUserMetadata(
+            currentClerkUser?.id.toString() !== undefined
+              ? currentClerkUser?.id.toString()
+              : "",
+            {
+              publicMetadata: {
+                userId: newUser._id,
+              },
+            }
+          );
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+export async function SyncUserUpdate() {
+  currentUser()
+    .then(async (currentClerkUser) => {
+      var id =
+        currentClerkUser?.id.toString() !== undefined
+          ? currentClerkUser?.id.toString()
+          : "id";
+      const user = {
+        username: currentClerkUser?.username
+          ? currentClerkUser?.username!
+          : "username",
+        firstName: currentClerkUser?.firstName
+          ? currentClerkUser?.firstName
+          : "firstName",
+        lastName: currentClerkUser?.lastName
+          ? currentClerkUser?.lastName!
+          : "lastName",
+        photo: currentClerkUser?.imageUrl
+          ? currentClerkUser?.imageUrl!
+          : "photo",
+      };
+
+      const updatedUser = await updateUser(id, user);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
